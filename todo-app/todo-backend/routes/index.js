6d0bafc7c1434ab777d2791ecc0969532a4cb210
Sushant Bajracharya <bajra.sushant@gmail.com>
2024-07-01 15:28:37 +0545

    12.12 done
    ramailo raicha docker

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const redis = require("../redis");
+const express = require("express");
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
+  });
+});
+
+router.get("/statistics", async (req, res) => {
+  let todo_count = await redis.getAsync("added_todos");
+  if(!todo_count) { todo_count = 0 }
+  res.send({
+    added_todos: parseInt(todo_count),
   });
 });
 
